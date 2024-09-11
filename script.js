8473a5031d77504ceb8d6b188e7294cd24c55558
Neeraj Shrivas <shrivasneeraj01@gmail.com>
2024-09-11 15:20:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+	
 function getFormvalue() {
     //Write your code here
+	let firstname= document.querySelector(".onename").value;
+	let lastname= document.querySelector(".secondname").value;
+	// let btn= document.querySelector(".submitbtn");
+	
+	// btn.addEventListener("click", getFormvalue)
 
+	alert(firstname + lastname)
 }
